3f7811d0c32831650ec457ccfe4b5077a1e31aa1
Tagir Sharipov <t.sharipov@nexidigital.eu>
2023-04-19 15:51:04 +0200

    tested commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,8 @@
 import style from './Button.module.css'
 
 const  Button: React.FC<{handler: () => void, children: string, disabled?: boolean}> = (props) => {
-  return <button type='button' disabled={props.disabled} className={style.button} onClick={props.handler}>{props.children}</button>
+  return <button type='button' 
+    disabled={props.disabled} className={style.button} onClick={props.handler}>{props.children}</button>
 }
 
 export default Button;
